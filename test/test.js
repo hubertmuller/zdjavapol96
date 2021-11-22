648984868c4aa97179e8356c7f1c14dd41477cd8
Hubert Muller <hubert@sportport.eu>
2021-11-22 20:52:44 +0100

    sass/ts

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
-console.log('Czesc, witajcie');
-
-window.history;
+var x = /** @class */ (function () {
+    function x() {
+        this.imie = 'Adam';
+    }
+    return x;
+}());
+var osoba = new x();
+console.log('Czesc, witajcie', osoba.imie);
---
+++ test/test.ts
@@ -0,0 +1,14 @@
+interface A {
+
+}
+
+class x {
+    public imie: string;
+    constructor() {
+        this.imie = 'Adam';
+    }
+}
+
+let osoba = new x();
+
+console.log('Czesc, witajcie', osoba.imie);
