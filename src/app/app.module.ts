525bbd5c00a1c0af3c31f1bf91439ec9c36f332b
Hubert Muller <hubert@sportport.eu>
2021-11-23 19:39:31 +0100

    stopka i naglowek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NaglowekComponent } from './naglowek/naglowek.component';
+import { StopkaComponent } from './stopka/stopka.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NaglowekComponent,
+    StopkaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stopka/stopka.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StopkaComponent } from './stopka.component';
+
+describe('StopkaComponent', () => {
+  let component: StopkaComponent;
+  let fixture: ComponentFixture<StopkaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StopkaComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StopkaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
